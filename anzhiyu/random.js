e745244f37b6e6e33244f27e22438a33b69e2380
Firstname-05 <76861103@qq.com>
2025-06-18 12:37:45 +0800

    Site updated: 2025-06-18 12:37:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/17/hello-world/","2025/06/18/翰鸽航空/"];function toRandomPost(){
+var posts=["2025/06/17/hello-world/","2025/06/18/翰鸽植保无人机/","2025/06/18/翰鸽航空/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
